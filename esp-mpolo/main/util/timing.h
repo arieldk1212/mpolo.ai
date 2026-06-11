c03e8d80e11ecdf6245b1849d55c6b90e48ffa26
arieldk1212 <arieldk6@gmail.com>
2026-06-11 09:26:01 +0300

    esp folder changed

---
+++ esp-mpolo/main/util/timing.h
@@ -0,0 +1,19 @@
+#ifndef TIME_H_
+#define TIME_H_
+
+#include <string>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/FreeRTOSConfig.h"
+
+namespace rt {
+
+static std::string GetTime() {
+  auto current_ticks = xTaskGetTickCount();
+  uint32_t time_ms = (current_ticks * 1000) / configTICK_RATE_HZ;
+  return std::to_string(time_ms);
+}
+
+}  // namespace rt
+
+#endif
